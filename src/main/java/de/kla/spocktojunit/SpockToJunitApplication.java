a97a8b98d8c08c7467f9347c84433e616f97d84e
Kevin <kevin.lange.1991@gmail.com>
2020-04-26 12:36:14 +0200

    initial spring files

---
+++ src/main/java/de/kla/spocktojunit/SpockToJunitApplication.java
@@ -0,0 +1,13 @@
+package de.kla.spocktojunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpockToJunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpockToJunitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/kla/spocktojunit/SpockToJunitApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kla.spocktojunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpockToJunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
